04a86f10950b051a3e7ae114aad1e9345731ff32
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2018-07-19 17:54:48 +0530

    Added date classification

--- js/popup.js
+++ js/popup.js
@@ -2,12 +2,58 @@ let list = document.getElementById("list");
 
 // populate the popup
 let historyItems = ExtensionState.GetAllHistoryItems();
-for (let i = historyItems.length - 1; i >= 0; --i)
+
+let historyToday = [];
+let historyLastWeek = [];
+let historyLastMonth = [];
+
+let currentDate = new Date();
+let currentTimeInMillis = currentDate.getTime();
+
+historyItems.map((historyItem) =>
 {
-    mainStuff.innerHTML += `
-    <div class="col-1">${historyItems[i].id}</div>
-    <div class="col-1">${historyItems[i].parentId}</div>
-    <div class="col-10"><p style="overflow:hidden;">${historyItems[i].url}</p></div>
+    switch (Math.ceil((currentTimeInMillis - historyItem.timeStamp) / 86400000))
+    {
+        case 1:
+            historyToday.push(historyItem);
+            break;
+        case 7:
+            historyLastWeek.push(historyItem);
+            break;
+        case 30:
+            historyLastMonth.push(historyItem);
+            break;
+        default:
+            historyLastMonth.push(historyItem);
+            break;
+    }
+})
+
+// I know I could've just written a function but meh
+
+for (let i = historyToday.length - 1; i >= 0; --i)
+{
+    todayHistory.innerHTML += `
+    <div class="col-1">${historyToday[i].id}</div>
+    <div class="col-1">${historyToday[i].parentId}</div>
+    <div class="col-10"><p style="overflow:hidden;">${historyToday[i].url}</p></div>
     `
 }
 
+for (let i = historyLastWeek.length - 1; i >= 0; --i)
+{
+    thisWeekHistory.innerHTML += `
+    <div class="col-1">${historyLastWeek[i].id}</div>
+    <div class="col-1">${historyLastWeek[i].parentId}</div>
+    <div class="col-10"><p style="overflow:hidden;">${historyLastWeek[i].url}</p></div>
+    `
+}
+
+for (let i = historyLastMonth.length - 1; i >= 0; --i)
+{
+    lastMonthHistory.innerHTML += `
+    <div class="col-1">${historyLastMonth[i].id}</div>
+    <div class="col-1">${historyLastMonth[i].parentId}</div>
+    <div class="col-10"><p style="overflow:hidden;">${historyLastMonth[i].url}</p></div>
+    `
+}
