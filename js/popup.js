4a586ed85cfa25b1487775bdfc536f4c72a64165
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2018-07-19 16:05:07 +0530

    Removed table, added div instead

--- js/popup.js
+++ js/popup.js
@@ -4,10 +4,10 @@ let list = document.getElementById("list");
 let historyItems = ExtensionState.GetAllHistoryItems();
 for (let i = historyItems.length - 1; i >= 0; --i)
 {
-    let itemHtml = "<tr>";
-    itemHtml += "<td class=\"td1\">" + historyItems[i].id + "</td>";
-    itemHtml += "<td class=\"td2\">" + historyItems[i].parentId + "</td>"
-    itemHtml += "<td class=\"td3\">" + " <a href=" + historyItems[i].url + ">" + historyItems[i].url + "</a></td>"
-    mainTable.innerHTML += itemHtml;
+    mainStuff.innerHTML += `
+    <div class="col-1">${historyItems[i].id}</div>
+    <div class="col-1">${historyItems[i].parentId}</div>
+    <div class="col-10"><p style="overflow:hidden;">${historyItems[i].url}</p></div>
+    `
 }
 
